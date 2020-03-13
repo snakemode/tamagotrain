96a59aef75b09386d4f51dc59e8c32319cbdf60c
Glitch (hello-express) <none>
2020-03-13 19:49:07 +0000

    🎷☔️ Checkpoint ./public/script.js:7701387/1412

--- public/script.js
+++ public/script.js
@@ -15,21 +15,49 @@ class Poop extends Problem {
   }
 }
 
+class Vomit extends Problem {
+  constructor() {
+    super();
+  }
+}
+
+class PassedOutTraveller extends Problem {
+  constructor() {
+    super();
+  }
+}
+
 class Traveller {
   
 }
 
 class Platform {
-  constructor() {
+  constructor(platformId) {
+    this.platformId = platformId;
     this.capacity = 100;
+    this.temperature = 15;
     this.occupancy = [];
   }
 }
 
 
-
 class Game {
   constructor() {
     this.platforms = [];  
+  }
+  
+  tick() {
+    // move any departing trains off platforms
+    // move any arriving trains onto platforms
+    // apply effects of problems
+    // handle user input actions    
+  }
+  
+  registerArrival() {
+    // put the train into the arrival queue to shift onto a platform
+  }
+  
+  registerDeparture() {
+    // remove a train from a platform
   }
 }
