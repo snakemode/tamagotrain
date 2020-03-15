1748a7ef255388dfd877ca5dbc2d4bd1de29e502
Glitch (hello-express) <none>
2020-03-15 16:56:33 +0000

    🦃🍗 Checkpoint ./public/game-platform.js:7701387/1182 ./public/game-entities.js:7701387/506 ./public/game-train.js:7701387/616 ./views/index.html:7701387/267

--- public/game-platform.js
+++ public/game-platform.js
@@ -1,3 +1,5 @@
+/* globals Train */
+
 class Platform {
   constructor(id) {
     this.id = id;
@@ -8,6 +10,7 @@ class Platform {
     this.hygiene = 100;
     
     this.contents = [];
+    this.train = null;
     this.hasTrain = false;
     
     this.unprocessedMessages = [];
@@ -21,17 +24,28 @@ class Platform {
       
       if (msg.arrived) {
         this.hasTrain = true;
+        this.train = new Train();
         console.log(`A train just arrived on ${this.id}`);
       }      
             
       if (msg.departed) {
         this.hasTrain = false;
+        this.train = null;
         console.log(`A train just departed on ${this.id}`);
       }      
     }
     
-
+    if (this.train) {
+      this.train.tick(this);
+    }
+    
     // apply effects of problems
+    for (let item in this.contents) {
+      if (item["tick"]) {
+        item.tick(this);
+      }
+    }
+    
   }
 }
 
---
+++ public/game-train.js
@@ -0,0 +1,23 @@
+class Train {
+  constructor() {
+    this.ticks = 0;
+    this.hasTicked = false;
+  }
+  
+  tick(platform) {
+    this.ticks++;
+    
+    if(this.hasTicked) {
+      return; // Only run logic once.
+    }
+    
+    // Code that can generate problems on the platform goes here.
+    
+    
+    this.hasTicked = true;
+  }
+}
+
+if (typeof(module) != 'undefined') {
+  module.exports = { Train }
+}
