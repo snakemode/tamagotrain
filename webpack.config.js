6de68cb95f518b85ce5177e7dea8ce6af527ff9c
Glitch (trainagotchi) <none>
2020-03-26 10:57:26 +0000

    🚨🛀 Checkpoint ./webpack.config.js:7701387/276

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+console.log(path.join(process.cwd(), 'public'));
+console.log(path.resolve(__dirname, 'public'));
+
 module.exports = {
+  mode: 'development',
   entry: { index: './public-src/script.js' },
   plugins: [
     // new CleanWebpackPlugin(['dist'])
   ],
   output: {
-    filename: 'packed-script.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    publicPath: ''
+    publicPath: "/public/"
   }
 };
