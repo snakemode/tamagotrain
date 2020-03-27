2e9fae3a45de057e442d5fabd1db25e838a57401
Glitch (trainagotchi) <none>
2020-03-27 13:57:38 +0000

    🍖🐚 Checkpoint ./server.js:7701387/2367

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 const express = require("express");
-const app = express();
+const fs = require("fs");
+const { promisify } = require('util');
+
 const ably = require('ably');
+const client = new ably.Realtime(process.env.ABLY_API_KEY);
+
+const readFileAsync = promisify(fs.readFile);
+const createTokenRequestAsync = promisify(fs.readFile);
+
+const app = express();
+
 
 app.use(express.static("public"));
 
-app.get("/", (request, response) => {
+app.get("/", async (request, response) => {
+  const tokenRequestData = await createTokenRequest({ clientId: 'trainagotchi' });  
+  
+  fs.readFile(__dirname + "/views/index.html", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+  
+  
+  const fileC = await readFileAsync(__dirname + "/views/index.html");
+  
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/ably-key", (request, response) => {  
-  
-  const client = new ably.Realtime(process.env.ABLY_API_KEY);
-  const result = client.auth.createTokenRequest({ clientId: 'bob' }, function(err, tokenRequest) {    
-      if(err) {
-        console.log('An error occurred; err = ' + err.message);
-      } else {
-        console.log('Success; token request = ' + tokenRequest);
-      }
-  });
-  
-  response.json(result);
-  //response.json({ value: process.env.ABLY_API_KEY });
-  
-});
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
-async function createTokenRequestPromise(client) {
+async function createTokenRequest(request) {
   return new Promise((resolve, reject) => {  
-    client.auth.createTokenRequest({ clientId: 'bob' }, function(err, tokenRequest) {    
+    client.auth.createTokenRequest(request, function(err, tokenRequest) {    
         if (err) { reject(err); } else { resolve(tokenRequest); }
     });
   });
 }
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
