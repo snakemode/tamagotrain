49da05a92418805dcdfdc10093e1fff2e278e0cf
Glitch (trainagotchi) <none>
2020-03-29 12:08:41 +0000

    👹🏉 Checkpoint ./views/index.html:7701387/136 ./server.js:7701387/750

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const ably = require("ably");
-
-const client = new ably.Realtime(process.env.ABLY_API_KEY);
-
+var Ably = require('ably/promises');
+const client = new Ably.Realtime(process.env.ABLY_API_KEY);
 
 const app = express();
 app.use(express.static("public"));
@@ -12,18 +10,9 @@ app.get("/", async (request, response) => {
 });
 
 app.get("/api/createTokenRequest", async (request, response) => {
-  const tokenRequestData = await createTokenRequest();
+  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'trainagotchi' });
   response.send(tokenRequestData);
 });
-
-async function createTokenRequest(request) {
-  request = request || { clientId: 'trainagotchi' };
-  return new Promise((resolve, reject) => {  
-    client.auth.createTokenRequest(request, function(err, tokenRequest) {    
-        if (err) { reject(err); } else { resolve(tokenRequest); }
-    });
-  });
-}
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
