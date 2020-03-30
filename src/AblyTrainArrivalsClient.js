01049da89947573869907d23f172efdc465d03c6
Glitch (trainagotchi) <none>
2020-03-30 13:55:51 +0000

    🎿🌷 Checkpoint ./src/script.js:7701387/18 ./src/AblyTrainArrivalsClient.js:7701387/441

--- src/AblyTrainArrivalsClient.js
+++ src/AblyTrainArrivalsClient.js
@@ -13,10 +13,21 @@ class AblyTrainArrivalsClient {
     const channelId = `[product:ably-tfl/tube]tube:${channelName}:940GZZLUKSX:arrivals`;
     const channel = await this._client.channels.get(channelId);
     await channel.attach();    
+        
+    const resultPage = await channel.history(); 
+    console.log(resultPage);
+    
     channel.subscribe(this.onSubscriptionMessage); 
   }
   
   onSubscriptionMessage(data) {
+    
+    // unpack ably message, and 
+    const arrivedMessage =  station: stationName, line: "platformId1", arrived: true };
+    /* callback({ station: stationName, line: "platformId1", arrived: true });
+    setTimeout(() => { callback({ station: stationName, line: "platformId1", departed: true }); }, interval);  
+    */
+    
     console.log(data); 
   }
 }
