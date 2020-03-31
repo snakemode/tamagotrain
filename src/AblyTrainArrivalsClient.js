dc521c27624a374faf0bc6979106749e2d961fe0
Glitch (trainagotchi) <none>
2020-03-31 13:41:13 +0000

    ⏱🌓 Checkpoint ./src/AblyTrainArrivalsClient.js:7701387/2501 ./src/script.js:7701387/18

--- src/AblyTrainArrivalsClient.js
+++ src/AblyTrainArrivalsClient.js
@@ -7,6 +7,8 @@ class AblyTrainArrivalsClient {
   
   async listenForEvents(stationName, callback) { 
     this._callback = callback;
+    setInterval(() => this.dispatchAnyMessagesDue(), 1000 * 1);
+    
     await this.subscribeToLine("940GZZLUKSX", "northern");
   }
   
@@ -15,10 +17,14 @@ class AblyTrainArrivalsClient {
     const channel = await this._client.channels.get(channelId);
     await channel.attach();    
         
-    const resultPage = await channel.history(); 
+    const resultPage = await channel.history({ untilAttach: true, limit: 1 });
     console.log(resultPage);
     
     channel.subscribe(this.onSubscriptionMessage); 
+  }
+  
+  dispatchAnyMessagesDue() {
+    console.log("dispatching");
   }
   
   onSubscriptionMessage(data) {
@@ -39,3 +45,72 @@ class AblyTrainArrivalsClient {
 }
 
 module.exports = AblyTrainArrivalsClient;
+
+
+/*
+
+Incoming data example.
+
+{
+    "name": "data",
+    "id": "7ARFsAk7ld:0:0",
+    "encoding": null,
+    "data": [{
+        "Id": "-1711142636",
+        "OperationType": 1,
+        "VehicleId": "000",
+        "NaptanId": "940GZZLUEUS",
+        "StationName": "Euston Underground Station",
+        "LineId": "northern",
+        "LineName": "Northern",
+        "PlatformName": "Southbound - Platform 2",
+        "Direction": "inbound",
+        "Bearing": "",
+        "DestinationNaptanId": "940GZZLUKNG",
+        "DestinationName": "Kennington Underground Station",
+        "Timestamp": "2020-03-31T13:24:31.0891267Z",
+        "TimeToStation": 171,
+        "CurrentLocation": "At Camden Town",
+        "Towards": "Kennington via CX",
+        "ExpectedArrival": "2020-03-31T13:27:22Z",
+        "TimeToLive": "2020-03-31T13:27:22Z",
+        "ModeName": "tube",
+        "Timing": {
+            "CountdownServerAdjustment": "00:00:00",
+            "Source": "0001-01-01T00:00:00",
+            "Insert": "0001-01-01T00:00:00",
+            "Read": "2020-03-31T13:24:31.091Z",
+            "Sent": "2020-03-31T13:24:31Z",
+            "Received": "0001-01-01T00:00:00"
+        }
+    }, {
+        "Id": "-1711142636",
+        "OperationType": 1,
+        "VehicleId": "000",
+        "NaptanId": "940GZZLUEUS",
+        "StationName": "Euston Underground Station",
+        "LineId": "northern",
+        "LineName": "Northern",
+        "PlatformName": "Northbound - Platform 1",
+        "Direction": "outbound",
+        "Bearing": "",
+        "DestinationNaptanId": "940GZZLUEGW",
+        "DestinationName": "Edgware Underground Station",
+        "Timestamp": "2020-03-31T13:24:31.0891267Z",
+        "TimeToStation": 411,
+        "CurrentLocation": "Between Embankment and Charing Cross",
+        "Towards": "Edgware via CX",
+        "ExpectedArrival": "2020-03-31T13:31:22Z",
+        "TimeToLive": "2020-03-31T13:31:22Z",
+        "ModeName": "tube",
+        "Timing": {
+            "CountdownServerAdjustment": "00:00:00",
+            "Source": "0001-01-01T00:00:00",
+            "Insert": "0001-01-01T00:00:00",
+            "Read": "2020-03-31T13:24:31.091Z",
+            "Sent": "2020-03-31T13:24:31Z",
+            "Received": "0001-01-01T00:00:00"
+        }
+    },
+    
+    */
